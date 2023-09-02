2da0985cbde2b9890a6a9abb535939bd991ffaed
Braxxian <killarneypc1@gmail.com>
2023-09-02 19:29:14 +0100

    finish modal on hero page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function modal() {
+    let modal = document.getElementById("how-to-play");
+    modal.style.display = "block";
+}
+function closeModal() {
+    let modal = document.getElementById("how-to-play");
+    modal.style.display = "none";
+}
