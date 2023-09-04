1169ac84d2d5494b8926f03ccaa750d9dccd0d31
Braxxian <killarneypc1@gmail.com>
2023-09-04 11:09:37 +0100

    created function to assemble card deck and load it in memory

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,18 @@ function closeModal() {
     let modal = document.getElementById("how-to-play");
     modal.style.display = "none";
 }
+window.onload = function () {
+    assembleDeck();
+}
+function assembleDeck() {
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const suits = ["C", "D", "H", "S"];
+    deck = [];
+    for (let suit in suits) {
+        for (let value in values) {
+            deck.push(values[value] + "-" + suits[suit]);
+        }
+    }
+    console.log(deck);
+
+}
